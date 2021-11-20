1792f5ef3ca7166b6935c8d4544015690666e8db
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-20 18:37:50 +0100

    Add navigation component

--- React/cinema-react/src/App.js
+++ React/cinema-react/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import Home from './Components/Home';
+import Movies from './Components/Movies/Movies';
+import Shows from './Components/Shows/Shows';
+import Calendar from './Components/Calendar/Calendar';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <div className="container-fluid">
+          <Link className="navbar-brand" to="/">Kino "Bajka"</Link>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+            <div className="navbar-nav">
+              <Link className="nav-link" to="/movies/">Filmy</Link>
+              <Link className="nav-link" to="/shows/">Seanse</Link>
+              <Link className="nav-link" to="/calendar/">Kalendarz</Link>
+            </div>
+          </div>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/movies/" element={<Movies />} />
+              <Route path="/shows/" element={<Shows />} />
+              <Route path="/calendar/" element={<Calendar />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
