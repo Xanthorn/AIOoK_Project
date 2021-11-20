e32291ef7653614f224ab29b5bea7db10d2608af
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-20 18:33:42 +0100

    Add blank calendar component

---
+++ React/cinema-react/src/Components/Calendar/Calendar.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Calendar = (props) => {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="display-4 text-center">Kalendarz</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Calendar;
