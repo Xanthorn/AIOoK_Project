b0b05267b2169256d735160016e664c9b63ac62f
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-20 18:25:45 +0100

    Add blank home component

---
+++ React/cinema-react/src/Components/Home.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Home = (props) => {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="display-4 text-center">Witaj w kinie "Bajka"</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home;
