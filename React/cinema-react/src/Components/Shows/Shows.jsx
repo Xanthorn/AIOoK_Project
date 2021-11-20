0f698a9b6466e0c63346b68efdca2a12e6b4c5c4
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-20 18:26:02 +0100

    Add blank shows component

---
+++ React/cinema-react/src/Components/Shows/Shows.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Shows = (props) => {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="display-4 text-center">Lista seansów</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Shows;
