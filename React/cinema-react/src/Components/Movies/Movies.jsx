b23da27b2342935cd54b999a6aed0f9fa00ba4ac
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-20 18:25:54 +0100

    Add blank movies component

---
+++ React/cinema-react/src/Components/Movies/Movies.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Movies = (props) => {
+
+    return (
+        <div className="container">
+            <div className="row">
+                <div className="col">
+                    <h1 className="display-4 text-center">Lista filmów</h1>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Movies;
