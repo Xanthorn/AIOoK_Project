d0219e6527162d5d6c59bfb1b8c3884256bd015b
Karol <michkarol.2000@wp.pl>
2021-11-23 10:56:37 +0100

    Change format of table in movies and shows components

--- React/cinema-react/src/Components/Movies/Movies.jsx
+++ React/cinema-react/src/Components/Movies/Movies.jsx
@@ -21,9 +21,9 @@ class Movies extends Component {
                         <table className="table table-hover">
                             <thead>
                                 <tr>
-                                    <th className="text-center">Tytuł</th>
-                                    <th className="text-center">Czas trwania</th>
-                                    <th className="text-center">Zarządzanie</th>
+                                    <th className="text-center col-7">Tytuł</th>
+                                    <th className="text-center col-5">Czas trwania</th>
+                                    <th className="text-center col-1">Zarządzanie</th>
                                 </tr>
                             </thead>
                             <tbody>
@@ -45,7 +45,10 @@ class Movies extends Component {
                 </div>
                 <div className="row">
                     <div className="col">
-                        TDO: Komponent do dodawania
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-circle" viewBox="0 0 16 16">
+                            <path d="M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z" />
+                            <path d="M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z" />
+                        </svg>
                     </div>
                 </div>
             </div>
