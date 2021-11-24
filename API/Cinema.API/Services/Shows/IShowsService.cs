bb61b590fdb3562869c12e971d86169b7820967e
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-24 13:44:23 +0100

    Add shows service interface

---
+++ API/Cinema.API/Services/Shows/IShowsService.cs
@@ -0,0 +1,11 @@
+﻿using Cinema.API.Contracts.Requests.Shows;
+using Cinema.API.Contracts.Responses.Shows;
+using System.Threading.Tasks;
+
+namespace Cinema.API.Services.Shows
+{
+    public interface IShowsService
+    {
+        Task<GetShowsByDayResponse> GetShowsByDay(GetShowsByDayRequest request);
+    }
+}
