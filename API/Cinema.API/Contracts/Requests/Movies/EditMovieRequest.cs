de3a1c2b1532e4a94380e93265778a0cc311e4d3
Karol <michkarol.2000@wp.pl>
2021-11-25 17:31:36 +0100

    Delete unused using directives

--- API/Cinema.API/Contracts/Requests/Movies/EditMovieRequest.cs
+++ API/Cinema.API/Contracts/Requests/Movies/EditMovieRequest.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Cinema.API.Contracts.Requests.Movies
+﻿namespace Cinema.API.Contracts.Requests.Movies
 {
     public class EditMovieRequest
     {
--- API/Cinema.API/Contracts/Responses/Movies/EditMovieResponse.cs
+++ API/Cinema.API/Contracts/Responses/Movies/EditMovieResponse.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Cinema.API.Contracts.Responses.Movies
+﻿namespace Cinema.API.Contracts.Responses.Movies
 {
     public class EditMovieResponse
     {
