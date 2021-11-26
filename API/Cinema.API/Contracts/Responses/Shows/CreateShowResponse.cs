53b3f36d76f8e527db1c455fd69be49396eb9ad9
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-26 11:09:53 +0100

    Add create show response

---
+++ API/Cinema.API/Contracts/Responses/Shows/CreateShowResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Cinema.API.Contracts.Responses.Shows
+{
+    public class CreateShowResponse
+    {
+        public Guid ShowId { get; set; }
+        public string Message { get; set; }
+        public int ErrorCode { get; set; }
+    }
+}
