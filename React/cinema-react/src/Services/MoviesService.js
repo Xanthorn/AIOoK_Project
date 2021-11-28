d479d1bf5740a52e6d17a1b9e884429daf7f7361
Karol <michkarol.2000@wp.pl>
2021-11-28 20:40:08 +0100

    #3 added check if response exist in MovieService

--- React/cinema-react/src/Services/MoviesService.js
+++ React/cinema-react/src/Services/MoviesService.js
@@ -4,14 +4,18 @@ const url = "https://localhost:5001/api/v1/movies/"; // Change "localhost:44382"
 
 export default class MoviesService {
 
+
     async getMovies() {
+        
         try {
             const response = await axios.get(`${url}`);
 
             return response.data;
         }
         catch (e) {
-            console.error(e.response.data);
+            if(e.response !== undefined) {
+                console.error(e.response.data);
+            }
             return [];
         }
     }
