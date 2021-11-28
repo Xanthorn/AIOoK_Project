09f8e5c05307d645f91b7be42df935695b7086b1
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-28 20:49:50 +0100

    Add MoviesService with createMovie()

---
+++ React/cinema-react/src/Services/MoviesService.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const url = "https://localhost:44382/api/v1/movies"; // Change "localhost:44382" to your own api adress
+
+export default class MoviesService {
+    async createMovie(title, hours, minutes) {
+        try {
+            const response = await axios.post(url, {
+                title: title,
+                durationHours: Number(hours),
+                durationMinutes: Number(minutes),
+            });
+            console.log(`Movie ID: ${response.data}`);
+        }
+        catch (e) {
+            if(e.response !== undefined) {
+                console.error(e.response.data);
+            }
+        }
+    }
+}
