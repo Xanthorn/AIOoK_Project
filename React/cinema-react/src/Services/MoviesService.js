feeaba3cd519ba61e0839a51dc53578bafb5aa2d
Karol <michkarol.2000@wp.pl>
2021-11-28 20:25:53 +0100

    #3 create movie serwis

---
+++ React/cinema-react/src/Services/MoviesService.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const url = "https://localhost:5001/api/v1/movies/"; // Change "localhost:44382" to your own api adress
+
+export default class MoviesService {
+
+    async getMovies() {
+        try {
+            const response = await axios.get(`${url}`);
+
+            return response.data;
+        }
+        catch (e) {
+            console.error(e.response.data);
+            return [];
+        }
+    }
+
+}
