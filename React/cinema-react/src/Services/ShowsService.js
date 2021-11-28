243963780b4864c20dd5cf1c7d9085b251dc2177
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-11-28 20:49:27 +0100

    Remove empty space in Shows Service

--- React/cinema-react/src/Services/ShowsService.js
+++ React/cinema-react/src/Services/ShowsService.js
@@ -3,11 +3,6 @@ import axios from "axios";
 const url = "https://localhost:44382/api/v1/shows"; // Change "localhost:44382" to your own api adress
 
 export default class ShowsService {
-
-
-
-
-
     async getShowsByDate(year, month, day) {
         try {
             const response = await axios.get(`${url}/${year}/${month}/${day}`);
