944b0d238383fca1c2009e84585654001837931e
Karol <michkarol.2000@wp.pl>
2021-12-02 15:55:24 +0100

    #8 Component movie edit change

--- React/cinema-react/src/Components/Movies/EditMovie.jsx
+++ React/cinema-react/src/Components/Movies/EditMovie.jsx
@@ -1,57 +1,63 @@
-import React, { Component } from "react";
+import { useParams } from "react-router-dom";
+import { useState } from "react";
+import MoviesService from "../../Services/MoviesService";
+import { useNavigate } from "react-router";
 
-class EditMovie extends Component {
-    constructor(props) {
-        super(props);
-        let movie = 0;
-        let i;
-        for (i = 0; i < this.props.movies.length; i++) {
-            if (this.props.movies[i].id === this.props.id) {
-                movie = this.props.movies[i];
-                break;
-            }
-        }
-        this.state = {
-            name: movie.name,
-            duration: movie.duration,
-            index: i
-        };
+export default function EditMovie() {
+    const { id } = useParams();
+    const [title, setTitle] = useState("");
+    const [hours, setHours] = useState(0);
+    const [minutes, setMinutes] = useState(0);
+    const navigate = useNavigate();
+
+    async function editMovie() {
+        const moviesService = new MoviesService();
+        await moviesService.editMovie(id, title, hours, minutes);
+        navigate("/movies");
     }
 
-    editMovie() {
-        this.props.movies[this.state.index].name = this.state.name;
-        this.props.movies[this.state.index].duration = this.state.duration;
-    }
-
-    onChange(e) {
-        let target = e.target.id;
-        this.setState({
-            [target]: e.target.value
-        });
-    }
-
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <div className="col text-center">
-                        <h3>Edycja filmu</h3>
-                    </div>
-                </div>
-                <div className="row">
-                    <div className="col">
-                        <input type="text" className="form-control form-control-lg form" id="name" placeholder="Tytuł filmu" value={this.state.name} onChange={(e) => this.onChange(e)} />
-                    </div>
-                    <div className="col">
-                        <input type="text" className="form-control form-control-lg form" id="duration" placeholder="Czas trwania" value={this.state.duration} onChange={(e) => this.onChange(e)} />
-                    </div>
-                    <div className="col">
-                        <button className="btn btn-lg btn-outline-dark" onClick={() => this.editMovie()}>Edytuj filmu</button>
+    return (
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-8">
+                    <div className="container border rounded mt-4">
+                        <div className="row mt-4 mb-4">
+                            <div className="col">
+                                <h1 className="text-center display-4">Edytuj film</h1>
+                            </div>
+                        </div>
+                        <div className="row justify-content-center">
+                            <div className="col-10">
+                                <div className="container">
+                                    <div className="row mb-3">
+                                        <div className="col">
+                                            <div className="form-floating">
+                                                <input type="text" className="form-control" id="title" placeholder="movie name" onChange={e => setTitle(e.target.value)} />
+                                                <label htmlFor="title">Tytuł filmu</label>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row mb-3">
+                                        <div className="col">
+                                            <label htmlFor="hours" className="form-label">Czas trwania</label>
+                                            <div className="input-group">
+                                                <input type="text" className="form-control" placeholder="hours" id="hours" onChange={e => setHours(e.target.value)} />
+                                                <span className="input-group-text">:</span>
+                                                <input type="text" className="form-control" placeholder="minutes" id="minutes" onChange={e => setMinutes(e.target.value)} />
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="row mb-4">
+                                        <div className="col text-center">
+                                            <button className="btn btn-outline-secondary" onClick={() => editMovie()}>Edytuj</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
-
-export default EditMovie;
