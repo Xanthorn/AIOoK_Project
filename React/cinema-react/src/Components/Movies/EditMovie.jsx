8c9a7244a6be0c1e51075993ed26db2c957f771f
Karol <michkarol.2000@wp.pl>
2021-12-02 20:35:31 +0100

    #8 add fetching to edit movie component

--- React/cinema-react/src/Components/Movies/EditMovie.jsx
+++ React/cinema-react/src/Components/Movies/EditMovie.jsx
@@ -1,5 +1,5 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MoviesService from "../../Services/MoviesService";
 import { useNavigate } from "react-router";
 
@@ -9,6 +9,23 @@ export default function EditMovie() {
     const [hours, setHours] = useState(0);
     const [minutes, setMinutes] = useState(0);
     const navigate = useNavigate();
+    const [movie, setMovie] = useState();
+
+    useEffect(function effectFunction() {
+        async function fetchMovie() {
+           
+            const moviesService = new MoviesService();
+
+            let fetchedMovie = await moviesService.getMovieById(id);
+
+            if (fetchedMovie != null) {
+                setMovie(fetchedMovie);
+            }
+        }
+
+        fetchMovie();
+
+    }, [id, setMovie]);
 
     async function editMovie() {
         const moviesService = new MoviesService();
@@ -23,7 +40,7 @@ export default function EditMovie() {
                     <div className="container border rounded mt-4">
                         <div className="row mt-4 mb-4">
                             <div className="col">
-                                <h1 className="text-center display-4">Edytuj film</h1>
+                                <h1 className="text-center display-4">Edytuj film {movie != null ? (movie.title) : (<h2>...</h2>) }</h1>
                             </div>
                         </div>
                         <div className="row justify-content-center">
@@ -33,7 +50,7 @@ export default function EditMovie() {
                                         <div className="col">
                                             <div className="form-floating">
                                                 <input type="text" className="form-control" id="title" placeholder="movie name" onChange={e => setTitle(e.target.value)} />
-                                                <label htmlFor="title">Tytuł filmu</label>
+                                                <label htmlFor="title">{movie != null ? (movie.title) : (<h2>...</h2>) }</label>
                                             </div>
                                         </div>
                                     </div>
@@ -41,9 +58,9 @@ export default function EditMovie() {
                                         <div className="col">
                                             <label htmlFor="hours" className="form-label">Czas trwania</label>
                                             <div className="input-group">
-                                                <input type="text" className="form-control" placeholder="hours" id="hours" onChange={e => setHours(e.target.value)} />
+                                                <input type="text" className="form-control" placeholder={movie != null ? (movie.durationHours) : (<h2>...</h2>)}  id="hours" onChange={e => setHours(e.target.value)} />
                                                 <span className="input-group-text">:</span>
-                                                <input type="text" className="form-control" placeholder="minutes" id="minutes" onChange={e => setMinutes(e.target.value)} />
+                                                <input type="text" className="form-control" placeholder={movie != null ? (movie.durationMinutes) : (<h2>...</h2>)}  id="minutes" onChange={e => setMinutes(e.target.value)} />
                                             </div>
                                         </div>
                                     </div>
