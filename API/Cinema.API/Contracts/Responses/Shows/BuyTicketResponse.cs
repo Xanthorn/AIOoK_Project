91fe34345d4bcd42ebb0c0ddadb435cf63d6285e
Karol <michkarol.2000@wp.pl>
2021-12-03 18:56:02 +0100

    #41 add buy ticket response

---
+++ API/Cinema.API/Contracts/Responses/Shows/BuyTicketResponse.cs
@@ -0,0 +1,12 @@
+﻿using Cinema.API.Contracts.Responses.Common;
+using System;
+using System.Collections.Generic;
+
+namespace Cinema.API.Contracts.Responses.Shows
+{
+    public class BuyTicketResponse
+    {
+        public List<Guid> SeatsId { get; set; }
+        public ErrorResponse ErrorResponse { get; set; }
+    }
+}
