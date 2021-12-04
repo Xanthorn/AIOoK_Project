326c08f5578b38a40307924b0faaba1b91f267b5
Karol <michkarol.2000@wp.pl>
2021-12-04 17:40:05 +0100

    #41 Add buy tickets request

---
+++ API/Cinema.API/Contracts/Requests/Shows/BuyTicketsRequest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Cinema.API.Contracts.Requests.Shows
+{
+    public class BuyTicketsRequest
+    {
+        public Guid ShowId { get; set; }
+        public List<Guid> SeatsId { get; set; }
+    }
+}
