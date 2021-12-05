d6dd02931296a246e7b4a47d90a479e044703a0e
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2021-12-05 02:51:19 +0100

    Add request and response

---
+++ API/Cinema.API/Contracts/Requests/Movies/GetPopularityOfMovieByDateRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Cinema.API.Contracts.Requests.Movies
+{
+    public class GetPopularityOfMovieByDateRequest
+    {
+        [JsonIgnore]
+        public Guid Id { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
---
+++ API/Cinema.API/Contracts/Responses/Movies/GetPopularityOfMovieByDateResponse.cs
@@ -0,0 +1,13 @@
+﻿using Cinema.API.Contracts.Responses.Common;
+
+namespace Cinema.API.Contracts.Responses.Movies
+{
+    public class GetPopularityOfMovieByDateResponse
+    {
+        public int NumberOfShows { get; set; } = 0;
+        public int SumOfTickets { get; set; } = 0;
+        public int AvailableTickets { get; set; } = 0;
+        public int SoldTickets { get; set; } = 0;
+        public ErrorResponse ErrorResponse { get; set; }
+    }
+}
