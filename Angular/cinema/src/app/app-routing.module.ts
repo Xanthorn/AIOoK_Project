2b82f99db335591bae141f4ca2e8fd38505f496e
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-18 11:08:24 +0100

    Create edit movie component

--- Angular/cinema/src/app/app-routing.module.ts
+++ Angular/cinema/src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MovieAddComponent } from './movies/movie-add/movie-add.component';
+import { MovieEditComponent } from './movies/movie-edit/movie-edit.component';
 import { MovieListComponent } from './movies/movie-list/movie-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'movies', component:MovieListComponent},
-  { path: 'movies/add', component: MovieAddComponent }
+  { path: 'movies/add', component: MovieAddComponent },
+  { path: 'movies/edit/:id', component: MovieEditComponent }
 ];
 
 @NgModule({
