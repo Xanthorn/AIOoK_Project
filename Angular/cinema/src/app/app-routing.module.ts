829818d75a3cedad161c4dec4427fdaf9157e352
Xanthorn <59312494+Xanthorn@users.noreply.github.com>
2022-01-18 10:18:59 +0100

    Create movie list component

--- Angular/cinema/src/app/app-routing.module.ts
+++ Angular/cinema/src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { MovieAddComponent } from './movies/movie-add/movie-add.component';
+import { MovieListComponent } from './movies/movie-list/movie-list.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
+  { path: 'movies', component:MovieListComponent},
   { path: 'movies/add', component: MovieAddComponent }
 ];
 
--- Angular/cinema/src/app/movies/movie/movie.component.ts
+++ Angular/cinema/src/app/movies/movie/movie.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Movie } from 'src/models/Movie';
 
 @Component({
-  selector: 'app-movie',
+  selector: '[app-movie]',
   templateUrl: './movie.component.html',
   styleUrls: ['./movie.component.css']
 })
