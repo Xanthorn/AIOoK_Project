9c55c9f39258c4749b87245194512ead1616d8da
Karol <michkarol.2000@wp.pl>
2022-01-19 17:22:23 +0100

    manually relocated movie edit component

---
+++ Angular/cinema/src/app/movies/movie-edit/movie-edit.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Movie } from 'src/models/Movie';
+
+@Component({
+  selector: 'app-movie-edit',
+  templateUrl: './movie-edit.component.html',
+  styleUrls: ['./movie-edit.component.css']
+})
+export class MovieEditComponent implements OnInit {
+
+  form: FormGroup;
+
+  movie: Movie = {
+    id: "1",
+    title: "Epoka Lodowcowa",
+    durationHours: 2,
+    durationMinutes: 3
+  };
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: [ this.movie.title, [
+        Validators.required,
+        Validators.maxLength(200)
+      ]],
+      hours: [ this.movie.durationHours, [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(9)
+      ]],
+      minutes: [ this.movie.durationMinutes, [
+        Validators.required,
+        Validators.min(0),
+        Validators.max(59)
+      ]]
+    })
+   }
+
+  ngOnInit(): void {
+  }
+
+  submitForm() {
+
+  }
+}
