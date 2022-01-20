32d2c9d0b86160a6b43b23cec1cbc583e1596887
Karol <michkarol.2000@wp.pl>
2022-01-20 12:22:47 +0100

    fix shows view

--- Angular/cinema/src/app/shows/show-details/show-details.component.ts
+++ Angular/cinema/src/app/shows/show-details/show-details.component.ts
@@ -2,7 +2,7 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Show } from 'src/models/Show';
 
 @Component({
-  selector: 'app-show-details',
+  selector: '[app-show-details]',
   templateUrl: './show-details.component.html',
   styleUrls: ['./show-details.component.css']
 })
