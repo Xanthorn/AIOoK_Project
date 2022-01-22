6dc7c452896bd23ab4c7379148abb21d7755881e
Karol <michkarol.2000@wp.pl>
2022-01-22 20:27:52 +0100

    #74 sorting added

--- Angular/cinema/src/app/movies/movie-list/movie-list.component.ts
+++ Angular/cinema/src/app/movies/movie-list/movie-list.component.ts
@@ -9,6 +9,8 @@ import { MoviesService } from 'src/services/movies.service';
 })
 export class MovieListComponent implements OnInit {
   movies: Movie[] = [];
+  alphabeticalTitle: boolean = false;
+  alphabeticalLength: boolean = false;
 
   constructor(private moviesService: MoviesService) { }
 
@@ -16,5 +18,24 @@ export class MovieListComponent implements OnInit {
     this.movies = await this.moviesService.getMovies();
     console.log("Movies was fetched correctly");
   }
-
+  public handleClickTitle() {
+    if(this.alphabeticalTitle == false){
+      this.movies.sort((a, b) => a.title.localeCompare(b.title));
+      this.alphabeticalTitle = true;
+      this.alphabeticalLength = false;
+    } else {
+      this.movies.sort((a, b) => b.title.localeCompare(a.title));
+      this.alphabeticalTitle = false;
+    }
+  }
+  public handleClickLength() {
+    if(this.alphabeticalLength == false){
+      this.movies.sort((first, second) => 0 - (first.durationHours > second.durationHours ? -1 : 1));
+      this.alphabeticalLength = true;
+      this.alphabeticalTitle = false;
+    } else {
+      this.movies.sort((first, second) => 0 - (first.durationHours > second.durationHours ? 1 : -1));
+      this.alphabeticalLength = false;
+    }
+  }
 }
--- Angular/cinema/src/app/shows/shows-list/shows-list.component.ts
+++ Angular/cinema/src/app/shows/shows-list/shows-list.component.ts
@@ -10,10 +10,82 @@ import { ShowsService } from 'src/services/shows.service';
 export class ShowsListComponent implements OnInit {
   shows: Show[] = [];
 
+  alphabeticalDate: boolean = false;
+  alphabeticalMovie: boolean = false;
+  alphabeticalAuditorium: boolean = false;
+  alphabeticalSoldTickets: boolean = false;
+  alphabeticalAvailableTickets: boolean = false;
+
   constructor(private showsService: ShowsService) { }
 
   async ngOnInit(): Promise<void> {
     this.shows = await this.showsService.getShows();
     console.log("Shows was fetched correctly");
+  }
+
+  public handleClickDate() {
+    if(this.alphabeticalDate == false){
+      this.shows.sort((a, b) => a.date.toString().localeCompare(b.date.toString()));
+      this.alphabeticalDate = true;
+      this.alphabeticalMovie = false;
+      this.alphabeticalAuditorium = false;
+      this.alphabeticalSoldTickets = false;
+      this.alphabeticalAvailableTickets = false;
+    } else {
+      this.shows.sort((a, b) => b.date.toString().localeCompare(a.date.toString()));
+      this.alphabeticalDate = false;
+    }
+  }
+  public handleClickMovie() {
+    if(this.alphabeticalMovie == false){
+      this.shows.sort((a, b) => a.movie.title.localeCompare(b.movie.title));
+      this.alphabeticalMovie = true;
+      this.alphabeticalDate = false;
+      this.alphabeticalAuditorium = false;
+      this.alphabeticalSoldTickets = false;
+      this.alphabeticalAvailableTickets = false;
+    } else {
+      this.shows.sort((a, b) => b.movie.title.localeCompare(a.movie.title));
+      this.alphabeticalMovie = false;
+    }
+  }
+  public handleClickAuditorium() {
+    if(this.alphabeticalAuditorium == false){
+      this.shows.sort((first, second) => 0 - (first.auditorium.number > second.auditorium.number ? -1 : 1));
+      this.alphabeticalAuditorium = true;
+      this.alphabeticalDate = false;
+      this.alphabeticalMovie = false;
+      this.alphabeticalSoldTickets = false;
+      this.alphabeticalAvailableTickets = false;
+    } else {
+      this.shows.sort((first, second) => 0 - (first.auditorium.number > second.auditorium.number ? 1 : -1));
+      this.alphabeticalAuditorium = false;
+    }
+  }
+  public handleClickAvailableTickets() {
+    if(this.alphabeticalAvailableTickets == false){
+      this.shows.sort((first, second) => 0 - (first.availableTickets > second.availableTickets ? -1 : 1));
+      this.alphabeticalAvailableTickets = true;
+      this.alphabeticalDate = false;
+      this.alphabeticalAuditorium = false;
+      this.alphabeticalMovie = false;
+      this.alphabeticalSoldTickets = false;
+    } else {
+      this.shows.sort((first, second) => 0 - (first.availableTickets > second.availableTickets ? 1 : -1));
+      this.alphabeticalAvailableTickets = false;
+    }
+  }
+  public handleClickSoldTickets() {
+    if(this.alphabeticalSoldTickets == false){
+      this.shows.sort((first, second) => 0 - (first.soldTickets > second.soldTickets ? -1 : 1));
+      this.alphabeticalSoldTickets = true;
+      this.alphabeticalDate = false;
+      this.alphabeticalAuditorium = false;
+      this.alphabeticalMovie = false;
+      this.alphabeticalAvailableTickets = false;
+    } else {
+      this.shows.sort((first, second) => 0 - (first.soldTickets > second.soldTickets ? 1 : -1));
+      this.alphabeticalSoldTickets = false;
+    }
   }
 }
