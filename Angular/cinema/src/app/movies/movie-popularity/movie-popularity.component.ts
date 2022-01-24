5ed063bf3b6e46035f64a4fad6be54693a550079
Karol <michkarol.2000@wp.pl>
2022-01-24 23:55:29 +0100

    component movie popularity work & date pipe changed

--- Angular/cinema/src/app/movies/movie-popularity/movie-popularity.component.ts
+++ Angular/cinema/src/app/movies/movie-popularity/movie-popularity.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GetPopularityOfMovieByDateRequest } from 'src/contracts/requests/Movies/GetPopularityOfMovieByDateRequest';
+import { Popularity } from 'src/contracts/responses/Movies/Popularity';
+import { Movie } from 'src/models/Movie';
+import { MoviesService } from 'src/services/movies.service';
 
 @Component({
   selector: 'app-movie-popularity',
@@ -6,10 +11,43 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-popularity.component.css']
 })
 export class MoviePopularityComponent implements OnInit {
+  form: FormGroup;
+  movies: Movie[] = [];
 
-  constructor() { }
+  selectDate: Date = new Date();
+  fetchedPopularityData?: Popularity;
+  request?: GetPopularityOfMovieByDateRequest;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder, private moviesService: MoviesService) {
+    this.form = this.fb.group({
+      id: ['', [
+        Validators.required,
+      ]],
+      date: [ '', [
+        Validators.required
+      ]]
+    });
+   }
+
+  async ngOnInit(): Promise<void> {
+    this.movies = await this.moviesService.getMovies();
+    console.log("Movies was fetched correctly");
   }
 
+  async find(){
+    this.request = Object.assign({}, this.form.value);
+    
+    if (this.request == undefined) {
+      return;
+    }
+    this.selectDate = this.request.date;
+
+    this.fetchedPopularityData = await this.moviesService.getPopularityOfMovieByDate(
+      this.request.id, 
+      new Date(this.request.date).getFullYear(), 
+      new Date(this.request.date).getMonth() +1, 
+      new Date(this.request.date).getDate());
+      console.log(this.fetchedPopularityData);
+      
+  }
 }
